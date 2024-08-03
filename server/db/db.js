848d5d5a4f8d8af360ea785690ee9d7ba873189e
nikaxucishvil1 <xucishvilin90@gmail.com>
2024-08-03 18:17:34 +0400

    hosted back

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_SECRET, { useNewUrlParser: true });
+    await mongoose.connect(process.env.MONGO_DB_SECRET, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    });
     console.log("connected to db");
   } catch (error) {
     console.log(error);
